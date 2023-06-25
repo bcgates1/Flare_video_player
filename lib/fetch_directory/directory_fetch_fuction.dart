173208e71927924c4a29b7b72f5d992ce7604ab9
Bharath Chandran <bcemailid@gmail.com>
2023-06-25 21:29:17 +0530

    Final commit before state management

--- lib/fetch_directory/directory_fetch_fuction.dart
+++ lib/fetch_directory/directory_fetch_fuction.dart
@@ -1,11 +1,14 @@
+import 'dart:developer';
+
 import 'package:fetch_all_videos/fetch_all_videos.dart';
 import 'package:flare_video_player/fetched_directory_lists.dart';
 
 Future videoFetch() async {
-  
   FetchAllVideos ob = FetchAllVideos();
   List videos = await ob.getAllVideos();
+  log('fetch finished');
   List fetchedFolders = [];
+  print(fetchedFolders);
   for (String elements in videos) {
     var folderPath = elements.split('/');
     fetchedFolders.add(folderPath[folderPath.length - 2]);
@@ -17,10 +20,10 @@ Future videoFetch() async {
     for (int j = 0; j < folderList.length; j++) {
       if (videos[i].toString().contains(folderList[j])) {
         folderMapList[folderList[j]] ??= [];
+
         folderMapList[folderList[j]]!.add(videos[i]);
       }
     }
   }
   videoList = videos;
-  
 }
